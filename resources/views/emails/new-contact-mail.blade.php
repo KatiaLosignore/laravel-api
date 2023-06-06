57bc376a7a735c5d8953cf6403473e64fd41b131
KatiaLosignore <katialos.kl@gmail.com>
2023-06-06 18:28:18 +0200

    added fix new-contact-mail

--- resources/views/emails/new-contact-mail.blade.php
+++ resources/views/emails/new-contact-mail.blade.php
@@ -1,10 +1,12 @@
-Ciao amministratote,
-hai ricevuto un nuovo contatto.
-Questi sono i dettagli:
+<strong>Ciao amministratote,</strong>
+<br>
 
+hai ricevuto un nuovo contatto. Questi sono i dettagli:
+<br><br>
+Nome: <em>{{$lead->name}}</em>
 
-{{$lead->name}}
+<br><br>
+Email: <em>{{$lead->email}}</em>
 
-{{$lead->email}}
-
-{{$lead->message}}
+<br><br>
+Messaggio: <em>{{$lead->message}}</em>
