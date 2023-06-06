b9028d9ce8d81ec99586688d03b146213536b70a
KatiaLosignore <katialos.kl@gmail.com>
2023-06-06 16:42:46 +0200

    addel model Lead, migrations e controller & mail

---
+++ app/Http/Controllers/Api/LeadController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Mail\NewContact;
+use App\Models\Lead;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+
+class LeadController extends Controller
+{
+    public function store(Request $request) {
+
+        $data = $request->all();
+
+        $new_lead = new Lead();
+        $new_lead->fill($data);
+        $new_lead->save();
+
+        $objectNewContact = new NewContact($new_lead);
+        
+        Mail::to('info@katialosignore.it')->send($objectNewContact);
+
+    }
+}
---
+++ resources/views/emails/new-contact-mail.blade.php
@@ -0,0 +1,10 @@
+Ciao amministratote,
+hai ricevuto un nuovo contatto.
+Questi sono i dettagli:
+
+
+{{$lead->name}}
+
+{{$lead->email}}
+
+{{$lead->message}}
