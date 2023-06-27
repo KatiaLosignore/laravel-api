e7d77acf76745d8cf5cd0a2eec050fdce5f93a3f
KatiaLosignore <katialos.kl@gmail.com>
2023-06-27 13:23:20 +0200

    added fix

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -44,7 +44,7 @@
                     <form class="form_delete_project" action="{{route('admin.projects.destroy', ['project' => $project->slug])}}" method="POST">
                       @csrf
                       @method('DELETE')
-                      <button type="submit" class="btn btn-danger">Elimina</button>
+                      <button type="submit" class="btn btn-danger">Delete</button>
                   </form>
 
                 </td>
